42630257fe8097c64ab32aaf994352aacc18a0a3
tsavlis <tsavlis.thanos@gmail.com>
2021-08-12 22:04:04 +0300

    initialize app

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-validation-error";
+
+const router = express.Router();
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be 4-20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+    const { email, password } = req.body;
+
+    console.log("creating usesr...");
+    throw new DatabaseConnectionError();
+    res.send({});
+  }
+);
+
+export { router as signupRouter };
